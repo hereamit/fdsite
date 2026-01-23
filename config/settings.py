4b2c1c0203381b90af7b0f18b9417ba626fc1263
amit <greenworld123.ab@gmail.com>
2026-01-22 22:16:49 -0600

    Fix STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "/media/"
